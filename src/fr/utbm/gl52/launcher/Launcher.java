7b646301d5f51ad689c45082fcd09e28db94eefa
Maxime GROLLEAU <grolleau.maxime@gmail.com>
2014-06-06 11:24:54 +0200

    Mavenisation du Projet

--- src/fr/utbm/gl52/launcher/Launcher.java
+++ src/fr/utbm/gl52/launcher/Launcher.java
@@ -2,8 +2,15 @@ package fr.utbm.gl52.launcher;
 
 import fr.utbm.gl52.gui.AppFrame;
 
+/**
+ * @author mgrollea
+ *
+ */
 public class Launcher {
 
+	/**
+	 * @param argv
+	 */
 	public static void main(String[] argv) {
 		
 		AppFrame mainFrame = new AppFrame();
